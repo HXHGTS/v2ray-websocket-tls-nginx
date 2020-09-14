a98a615d6eaf7e76fb6687df0dab6001500a842d
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2020-09-14 16:42:34 +0800

    Add files via upload

--- v2ray.c
+++ v2ray.c
@@ -108,8 +108,9 @@ int install_v2ray() {
         printf("检测到证书与私钥文件未按照规定方式放置于根目录，强制退出！\n");
         exit(0);
     }
-    printf("请输入已绑定此服务器ip的域名:");
-    scanf("%s", sni);
+    config = fopen("/usr/local/etc/v2ray/sni.conf", "r");
+    fscanf(config, "%s", sni);
+    fclose(config);
     system("setenforce 0");
     system("yum install -y curl unzip bind-utils epel-release nginx");
     system("wget https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh -O install-release.sh");
@@ -164,6 +165,11 @@ int install_v2ray() {
 
 int KernelUpdate() {
     if ((fopen("KernelUpdate.sh", "r")) == NULL) {
+        printf("请输入已绑定此服务器ip的域名:");
+        scanf("%s", sni);
+        config = fopen("/usr/local/etc/v2ray/sni.conf", "w");
+        fprintf(config, "%s", sni);
+        fclose(config);
         printf("正在升级新内核. . .\n");
         system("wget https://github.com/HXHGTS/TCPOptimization/raw/master/KernelUpdate.sh");
         system("chmod +x KernelUpdate.sh");
