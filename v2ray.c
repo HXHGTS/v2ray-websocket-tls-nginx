0f1d3d586c0c88b95e3267ecb5697339215caebc
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2020-12-13 04:32:11 +0800

    Update v2ray.c

--- v2ray.c
+++ v2ray.c
@@ -163,7 +163,7 @@ int install_v2ray() {
     system("setenforce 0");
     system("yum install -y curl unzip epel-release nginx bind-utils ntpdate qrencode");
     printf("正在同步时间. . .\n");
-    system("ntpdate -u time.windows.com");
+    system("ntpdate -u time.cloudflare.com");
     printf("正在运行v2ray安装脚本. . .\n");
     system("wget https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh -O install-release.sh");
     system("chmod +x install-release.sh");
